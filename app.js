db28b4b5e5b47710ed2e2904ca4da84a18aee271
Efilrofzaggin4 <maxime.kiniffo@3wa.io>
2023-06-12 17:29:13 +0200

    mise en place register , son ejs et de sa route

--- app.js
+++ app.js
@@ -15,11 +15,20 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 app.set('view options', { pretty: true });
 
+//pour l'utilisation du json à la réception des données formulaire
+app.use(express.json()) 
+app.use(express.urlencoded({ extended: true })) //bien penser à recréer la base de donnée avec char 36 pour id
+
 
 // app.use(express.static("public"));
 //appel du routeur
 app.use('/', router);
 
+app.use(function (req, res, next) {
+    res.locals.isAdmin = req.session.role === 'admin';
+
+    next();
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=>{
--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,6 @@
 import express from "express";
 
+
 const router = express.Router();
 
 
@@ -7,12 +8,20 @@ const router = express.Router();
 //appel des routers
 
 import HomeController from "../controllers/home.js";  //page d'accueil
-
-
+import {Register, RegisterSubmit} from '../controllers/register.js'; //, RegisterSubmit
 //liste des routes
 
 //HOME PAGE
 router.get('/', HomeController);
+
+//INSCRIPTION PAGE
+
+router.get('/register', Register);
+
+router.post('/register', RegisterSubmit);
+
+
+
 
 
 
